2d994348aaba1b154e960a0019004d01253f24ee
João Victor Marks <jovic.marks@gmail.com>
2022-02-06 18:12:33 -0300

    fix: UpdateUserAvatarService

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   jwt: {
-    secret: process.env.APP_SECRET || '2a3b51b8ae5b2540c9a59cc3fb31c531',
+    secret: '2a3b51b8ae5b2540c9a59cc3fb31c531' || process.env.APP_SECRET,
     expiresIn: '1d',
   },
 };
--- src/config/upload.ts
+++ src/config/upload.ts
@@ -23,7 +23,7 @@ interface IUploadConfig {
 }
 
 export default {
-  driver: process.env.STORAGE_DRIVER,
+  driver: 'disk' || process.env.STORAGE_DRIVER,
 
   tmpFolder,
   uploadsFolder: path.resolve(tmpFolder, 'uploads'),
